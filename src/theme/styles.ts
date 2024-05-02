265c0a01d29562d76bd8e4d1fd2e951036fbca5c
Olexandra Korol <korol.olexandra@gmail.com>
2024-05-02 10:48:32 +0300

    creations structure of project

---
+++ src/theme/styles.ts
@@ -0,0 +1,23 @@
+export const colors = Object.freeze({
+  white: '#ffffff',
+  neonWhite: '#FBFFFF',
+  black: '#000000',
+  transparent: 'transparent',
+  grey: 'grey',
+
+  //colors for debbuging:
+
+  debugColorIndigo: '#4B0082',
+  debugColorAquamarine: '#7FFFD4',
+  debugColorRoyalBlue: '#4169E1',
+  debugColorGreenYellow: '#ADFF2F',
+  debugColorPaleVioletRed: '#DB7093',
+  debugColorSienna: '#A0522D',
+})
+
+export const fontWeight = Object.freeze({
+  slim: '100',
+  normal: '400',
+  medium: '500',
+  bolt: '600',
+})
